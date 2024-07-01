df6c9b362bfc76c4bc16fa37207ce288db3bff24
Captain-Kim <brighthero@kakao.com>
2024-07-01 15:25:37 +0900

    bugfix: spreadsheet 페이지 콘솔 에러 해결

--- src/pages/SpreadSheet.tsx
+++ src/pages/SpreadSheet.tsx
@@ -38,7 +38,12 @@ const SpreadSheet = () => {
                                 <span className="mt-1 font-medium text-secondary-dark text-lg/normal">최근 업데이트 일: {latestDate ? new Date(latestDate).toLocaleDateString() : '데이터를 불러오는 중입니다...'}</span>
                             </h3>
                             <div className="relative flex flex-wrap items-center my-2">
-                                <a href="javascript:void(0)" className="inline-block text-[.925rem] font-medium leading-normal text-center align-middle cursor-pointer rounded-2xl transition-colors duration-150 ease-in-out text-light-inverse bg-light-dark border-light shadow-none border-0 py-2 px-5 hover:bg-secondary active:bg-light focus:bg-light">PDF 요청하기(준비중)</a>
+                                <button
+                                    onClick={() => alert('PDF 요청하기(준비중)')}
+                                    className="inline-block text-[.925rem] font-medium leading-normal text-center align-middle cursor-pointer rounded-2xl transition-colors duration-150 ease-in-out text-light-inverse bg-light-dark border-light shadow-none border-0 py-2 px-5 hover:bg-secondary active:bg-light focus:bg-light"
+                                >
+                                    PDF 요청하기(준비중)
+                                </button>
                             </div>
                         </div>
                         {/* end card header */}
@@ -77,8 +82,8 @@ const SpreadSheet = () => {
                                                             className="ml-auto relative text-secondary-dark bg-light-dark hover:text-primary flex items-center h-[25px] w-[25px] text-base font-medium leading-normal text-center align-middle cursor-pointer rounded-2xl transition-colors duration-200 ease-in-out shadow-none border-0 justify-center"
                                                         >
                                                             <span className="flex items-center justify-center p-0 m-0 leading-none shrink-0 ">
-                                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-4 h-4">
-                                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-4 h-4">
+                                                                    <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
                                                                 </svg>
                                                             </span>
                                                         </button>
