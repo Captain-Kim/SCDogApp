e53a57b5528f96c9bcdbbda85efd7c8ce1ca25b5
Captain-Kim <brighthero@kakao.com>
2024-08-05 10:42:24 +0900

    fix: useNotice.ts 빌드에러 주석처리

--- src/hooks/useNotice.ts
+++ src/hooks/useNotice.ts
@@ -1,11 +1,11 @@
-import { useQuery } from '@tanstack/react-query';
-import { fetchNoticeData } from '../api/api';
-import { Notice } from '../types/type';
+// import { useQuery } from '@tanstack/react-query';
+// import { fetchNoticeData } from '../api/api';
+// import { Notice } from '../types/type';
 
-export const useNotice = () => {
-    return useQuery<Notice, Error>({
-        queryKey: ['notice'],
-        queryFn:fetchNoticeData,
-        staleTime: Infinity
-    });
-};
+// export const useNotice = () => {
+//     return useQuery<Notice, Error>({
+//         queryKey: ['notice'],
+//         queryFn:fetchNoticeData,
+//         staleTime: Infinity
+//     });
+// };
